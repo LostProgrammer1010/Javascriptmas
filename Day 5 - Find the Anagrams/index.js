4ed723d32131054d0d233604d38e9171642a4b03
Meyer <dmeyer20@URMC-SH.Rochester.edu>
2024-12-07 20:42:51 -0500

    Adding result photos to see final product

---
+++ Day 5 - Find the Anagrams/index.js
@@ -0,0 +1,37 @@
+/* 
+This Christmas, you’ve been tasked with running an anagram quiz at 
+the family gathering.
+
+You have been given a list of anagrams, but you suspect that some 
+of the anagram pairs might be incorrect.
+
+Your job is to write a JavaScript function to loop through the array
+and filter out any pairs that aren’t actually anagrams.
+
+For this challenge, spaces will be ignored, so "Be The Helm" would 
+be considered a valid anagram of "Bethlehem".
+*/
+
+let anagrams = [
+  ["Can Assault", "Santa Claus"],
+  ["Refreshed Erudite Londoner", "Rudolf the Red Nose Reindeer"],
+  ["Frosty The Snowman", "Honesty Warms Front"],
+  ["Drastic Charms", "Christmas Cards"],
+  ["Congress Liar", "Carol Singers"],
+  ["The Tin Glints", "Silent Night"],
+  ["Be The Helm", "Betlehem"],
+  ["Is Car Thieves", "Christmas Eve"]
+];
+
+function findAnagrams(array){
+    
+  for (let i=0; i < array.length; i++) {
+      if (array[0].sort() != array[1].sort()) {
+          array.splice(i, 1);
+      }
+  }
+  console.log(array)
+  // write your code here
+}
+
+findAnagrams(anagrams);
